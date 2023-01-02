955f2857232e91bdd388344d9725b2d699939f0b
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2023-01-02 11:15:32 +0530

    Add files via upload

--- plugins/about.py
+++ plugins/about.py
@@ -2,12 +2,14 @@ import os
 from pyrogram import Client, filters
 token = os.environ.get('TOKEN','')
 botid = token.split(':')[0]
-from helper.database import botdata, find_one, total_user
+from helper.database import botdata, find_one, total_user,getid
+
 from helper.progress import humanbytes
+
 @Client.on_message(filters.private & filters.command(["about"]))
 async def start(client,message):
 	botdata(int(botid))
 	data = find_one(int(botid))
 	total_rename = data["total_rename"]
 	total_size = data["total_size"]
-	await message.reply_text(f"Total User :- {total_user()}\nBot :- <a href='https://t.me/LazyStar_BOT'>Lazy Star </a>\nCreater :- <a href='https://t.me/LazyDeveloper'>LazyDeveloper</a>\nLanguage :- Python3\nLibrary :- Pyrogram 2.0\nServer :- KOYEB\nTotal Renamed File :- {total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))}",quote=True)
+	await message.reply_text(f"{getid()}\n Total User :- {total_user()}\nBot :- <a href='https://t.me/LazyStar_BOT'>Lazy Star </a>\nCreater :- <a href='https://t.me/LazyDeveloper'>LazyDeveloper</a>\nLanguage :- Python3\nLibrary :- Pyrogram 2.0\nServer :- KOYEB\nTotal Renamed File :- {total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))}",quote=True)
