8f74fbefa043e249b9f382262b1c252f1a1f7822
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2023-01-03 20:16:40 +0530

    Added some extra features

---
+++ plugins/lazyusers.py
@@ -0,0 +1,23 @@
+import os 
+from pyrogram import Client, filters
+from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup)
+token = os.environ.get('TOKEN','')
+botid = token.split(':')[0]
+ADMIN = int(os.environ.get("ADMIN", ""))
+
+from helper.database import botdata, find_one, total_user,getid
+
+from helper.progress import humanbytes
+
+@Client.on_message(filters.private & filters.user(ADMIN)  & filters.command(["lazyusers"]))
+async def start(client,message):
+	botdata(int(botid))
+	data = find_one(int(botid))
+	total_rename = data["total_rename"]
+	total_size = data["total_size"]
+	id = str(getid())
+	ids = id.split(',')
+
+	await message.reply_text(f"{ids}\n\nTotal User :- {total_user()}\n\nTotal Renamed File :- {total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))}",quote=True,
+                             reply_markup= InlineKeyboardMarkup([[InlineKeyboardButton("🦋 Close Menu 🦋", callback_data="cancel")]]) 
+                             )
---
+++ plugins/upgrade.py
@@ -0,0 +1,62 @@
+"""lokaman"""
+from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
+from pyrogram import Client , filters
+
+@Client.on_callback_query(filters.regex('upgrade'))
+async def upgrade(bot,update):
+	text = """**Free Plan User**
+	Daily  Upload limit 1.2GB
+	Price 0
+	
+	**🪙 Silver Tier 🪙** 
+	Daily  Upload  limit 10GB
+	Price Rs 66  ind /🌎 0.8$  per Month
+	
+	**💫 Gold Tier 💫**
+	Daily Upload limit 50GB
+	Price Rs 100  ind /🌎 1.2$  per Month
+	
+	**💎 Diamond 💎**
+	Daily Upload limit 100GB
+	Price Rs 206  ind /🌎 2.5$  per Month
+	
+	
+	Pay Using Upi I'd ```7808912076@paytm```
+	
+	After Payment Send Screenshots Of 
+        Payment To Admin @mRiderDM"""
+	keybord = InlineKeyboardMarkup([[ 
+        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/mRiderDM")], 
+        			[InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9"),
+        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+	await update.message.edit(text = text,reply_markup = keybord)
+	
+
+@Client.on_message(filters.private & filters.command(["upgrade"]))
+async def upgradecm(bot,message):
+	text = """**Free Plan User**
+	Daily  Upload limit 1.2GB
+	Price 0
+	
+	**🪙 Silver Tier 🪙** 
+	Daily  Upload  limit 10GB
+	Price Rs 66  ind /🌎 0.8$  per Month
+	
+	**💫 Gold Tier 💫**
+	Daily Upload limit 50GB
+	Price Rs 100  ind /🌎 1.2$  per Month
+	
+	**💎 Diamond 💎**
+	Daily Upload limit 100GB
+	Price Rs 206  ind /🌎 2.5$  per Month
+	
+	
+	Pay Using Upi I'd ```7808912076@paytm```
+	
+	After Payment Send Screenshots Of 
+        Payment To Admin @mRiderDM"""
+	keybord = InlineKeyboardMarkup([[ 
+        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/mRiDerDM")], 
+        			[InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9"),
+        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+	await message.reply_text(text = text,reply_markup = keybord)
