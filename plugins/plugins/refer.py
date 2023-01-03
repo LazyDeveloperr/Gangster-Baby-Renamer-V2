409e3007c5287212d0c1f9f794ef83b43a804ad3
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2023-01-03 20:13:55 +0530

    Delete plugins/plugins directory

--- plugins/plugins/refer.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
-@Client.on_message(filters.private & filters.command(["refer"]))
-async def refer(client,message):
-    reply_markup = InlineKeyboardMarkup(
-       		[[ InlineKeyboardButton("Share Your Link" ,url=f"https://t.me/share/url?url=https://t.me/GangsterBaby_renamer_BOT?start={message.from_user.id}") ]   ])
-    await message.reply_text(f"Refer And Earn Get 100MB Upload Limit\nPer Refer 100MB\n Your Link :- https://t.me/LazyStar_BOT?start={message.from_user.id} ",reply_to_message_id = message.id,reply_markup=reply_markup,)
-    
