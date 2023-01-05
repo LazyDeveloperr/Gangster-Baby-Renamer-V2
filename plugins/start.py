df00e7daff2b0e3d2c133042dbce530810817414
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2023-01-05 10:03:37 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -133,125 +133,63 @@ async def send_doc(client, message):
                                                                     ]))
         await message.reply_text(text=f"🦋")
         return 
-
-
+       
         c_time = time.time()
-
-
-
+       
        if buy_date==None:
-
            LIMIT = 350
-
        else:
-
            LIMIT = 50
-
        then = used_date+ LIMIT
-
        left = round(then - c_time)
-
        conversion = datetime.timedelta(seconds=left)
-
        ltime = str(conversion)
-
        if left > 0:       	    
-
        	await message.reply_text(f"```Sorry Dude I am not only for YOU \n Flood control is active so please wait for {ltime}```",reply_to_message_id = message.id)
-
        else:
-
        		# Forward a single message
-
        		await client.forward_messages(log_channel, message.from_user.id, message.id)
-
        		await client.send_message(log_channel,f"User Id :- {user_id}")       		
-
        		media = await client.get_messages(message.chat.id,message.id)
-
        		file = media.document or media.video or media.audio 
-
        		dcid = FileId.decode(file.file_id).dc_id
-
        		filename = file.file_name
-
        		value = 2147483648
-
        		used_ = find_one(message.from_user.id)
-
        		used = used_["used_limit"]
-
        		limit = used_["uploadlimit"]
-
        		expi = daily - int(time.mktime(time.strptime(str(date_.today()), '%Y-%m-%d')))
-
        		if expi != 0:
-
        			today = date_.today()
-
        			pattern = '%Y-%m-%d'
-
        			epcho = int(time.mktime(time.strptime(str(today), pattern)))
-
        			daily_(message.from_user.id,epcho)
-
        			used_limit(message.from_user.id,0)			     		
-
        		remain = limit- used
-
        		if remain < int(file.file_size):
-
        		    await message.reply_text(f"Sorry! I can't upload files that are larger than {humanbytes(limit)}. File size detected {humanbytes(file.file_size)}\nUsed Daly Limit {humanbytes(used)} If U Want to Rename Large File Upgrade Your Plan ",reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("Upgrade 💰💳",callback_data = "upgrade") ]]))
-
        		    return
-
        		if value < file.file_size:
-
        		    if STRING:
-
        		        if buy_date==None:
-
        		            await message.reply_text(f" You Can't Upload More Then {humanbytes(limit)} Used Daly Limit {humanbytes(used)} ",reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("Upgrade 💰💳",callback_data = "upgrade") ]]))
-
        		            return
-
        		        pre_check = check_expi(buy_date)
-
        		        if pre_check == True:
-
        		            await message.reply_text(f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {humanize.naturalsize(file.file_size)}\n**Dc ID** :- {dcid}""",reply_to_message_id = message.id,reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename",callback_data = "rename"),InlineKeyboardButton("✖️ Cancel",callback_data = "cancel")  ]]))
-
        		            total_rename(int(botid),prrename)
-
        		            total_size(int(botid),prsize,file.file_size)
-
        		        else:
-
        		            await message.reply_text(f'Your Plane Expired On {buy_date}',quote=True)
-
        		            return
-
        		    else:
-
        		          	await message.reply_text("Can't upload files bigger than 2GB ")
-
        		          	return
-
        		else:
-
        		    filesize = humanize.naturalsize(file.file_size)
-
        		    fileid = file.file_id
-
        		    total_rename(int(botid),prrename)
-
        		    total_size(int(botid),prsize,file.file_size)
-
        		    await message.reply_text(f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {filesize}\n**Dc ID** :- {dcid}""",reply_to_message_id = message.id,reply_markup = InlineKeyboardMarkup(
-
        		[[ InlineKeyboardButton("📝 Rename",callback_data = "rename"),
-
        		InlineKeyboardButton("✖️ Cancel",callback_data = "cancel")  ]]))
-
-       		
-    
