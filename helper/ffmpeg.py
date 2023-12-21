3e54ce13d03d8d7196429a02a6b797c328d4578a
Viizetabdi <154399342+Viizetabdi@users.noreply.github.com>
2023-12-21 07:35:35 +0300

    Update ffmpeg.py

--- helper/ffmpeg.py
+++ helper/ffmpeg.py
@@ -6,8 +6,8 @@ from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 
 async def fix_thumb(thumb):
-    width = 735
-    height = 414
+    width = 739
+    height = 415
     try:
         if thumb != None:
             metadata = extractMetadata(createParser(thumb))
@@ -17,7 +17,7 @@ async def fix_thumb(thumb):
                 height = metadata.get("height")
                 Image.open(thumb).convert("RGB").save(thumb)
                 img = Image.open(thumb)
-                img.resize((414, height))
+                img.resize((415, height))
                 img.save(thumb, "JPEG")
     except Exception as e:
         print(e)
